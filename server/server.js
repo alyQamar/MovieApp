5f673c17afd5a51b93181dd2187f85db4e18e9cb
alyQamar <alyeqee@gmail.com>
2023-08-09 22:45:33 +0300

    added dev environment and logger middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv"); // setup environment variables
+const morgan = require("morgan"); // setup requests logger middleware
+
+dotenv.config({ path: "config.env" });
 
 // Express App
 const app = express();
 
-const PORT = 9000;
+// Middleware
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
