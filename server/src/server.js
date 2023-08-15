d1a5345556b0cc1092cfcfb54f1d1b1609a56907
alyQamar <alyeqee@gmail.com>
2023-08-15 15:12:30 +0300

    Added global customize errors middleware logic to improve error handling.

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const app = require("./app");
+const app = require("./middlewares/app");
 const dotenv = require("dotenv"); // setup environment variables
 const morgan = require("morgan"); // setup requests logger middleware
 const dbConnection = require("./config/database");
@@ -19,4 +19,12 @@ if (process.env.NODE_ENV === "development") {
 const PORT = process.env.PORT || 9000;
 server.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
+});
+
+// Handle rejections outside app (Express)
+process.on("unhandledRejection", (err) => {
+  console.error(`UnhandledRejection Error: ${err.name} | ${err.message}`);
+  server.close(() => {
+    console.error(`Server shutting down...`);
+  });
 });
