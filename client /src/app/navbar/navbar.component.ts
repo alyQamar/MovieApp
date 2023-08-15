27fe3fa74e4db4c07bae81406a55ff734df58844
Mohamed_Hegazy <asadmoahmed405@gmail.com>
2023-08-15 07:30:44 +0300

    navbar done and rename the file

---
+++ client /src/app/navbar/navbar.component.ts
@@ -0,0 +1,58 @@
+import { Component ,HostListener, Renderer2 } from '@angular/core';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+ 
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const nav :HTMLElement|null = document.querySelector('nav');
+    const items :any = document.querySelectorAll('.nav-link');
+    if (window.pageYOffset > 100) {
+      nav?.classList.add('bg-warning',);
+      items.forEach((element:HTMLElement) => {
+        element?.classList.replace('text-white','text-dark');
+      });
+
+    } else {
+      nav?.classList.remove('bg-warning', );
+      items.forEach((element:HTMLElement) => {
+        element?.classList.replace('text-dark','text-white');
+
+      });
+    }
+  }
+  // isNavbarScrolled: boolean = false;
+  // constructor(private renderer: Renderer2) {}
+
+  // @HostListener('window:scroll')
+  // onWindowScroll() {
+  //   if (window.pageYOffset > 100) {
+  //     this.isNavbarScrolled = true;
+  //     this.addClassesToNavbar();
+  //   } else {
+  //     this.isNavbarScrolled = false;
+  //     this.removeClassesFromNavbar();
+  //   }
+  // }
+
+  // private addClassesToNavbar() {
+  //   const nav = document.querySelector('nav');
+  //   this.renderer.addClass(nav, 'bg-dark');
+  //   this.renderer.addClass(nav, 'shadow');
+  // }
+
+  // private removeClassesFromNavbar() {
+  //   const nav = document.querySelector('nav');
+  //   this.renderer.removeClass(nav, 'bg-dark');
+  //   this.renderer.removeClass(nav, 'shadow');
+  // }
+
+  }
+
+
