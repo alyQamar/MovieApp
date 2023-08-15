e14be83d31346931c2a5ba9524c4731a374fd1fb
alyQamar <alyeqee@gmail.com>
2023-08-15 15:17:15 +0300

    Added categories routes

---
+++ server/src/routes/categoryRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {
+  getCategoryValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidator");
+
+const {
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../services/categoryService");
+
+const router = express.Router();
+router
+  .route("/")
+  .get(getCategories)
+  .post(createCategoryValidator, createCategory);
+router
+  .route("/:id")
+  .get(getCategoryValidator, getCategory)
+  .put(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
+module.exports = router;
